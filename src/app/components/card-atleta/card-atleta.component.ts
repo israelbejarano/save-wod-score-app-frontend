93615f2a2c4ba9d3c39a69e3e7f70d95341bb317
israel <israel.bejarano.a@gmail.com>
2020-12-04 23:56:09 +0100

    uso de servicio y abrir modal

--- src/app/components/card-atleta/card-atleta.component.ts
+++ src/app/components/card-atleta/card-atleta.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { EditarPerfilComponent } from '../editar-perfil/editar-perfil.component';
+import { Atleta } from '../../models/interfaces/atleta';
+import { AtletasService } from '../../services/atletas.service';
 
 @Component({
   selector: 'app-card-atleta',
@@ -7,12 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardAtletaComponent implements OnInit {
 
-  constructor() { }
+  atleta: Atleta;
 
-  ngOnInit() {}
+  constructor(private modalController: ModalController,
+              private atletasService: AtletasService) { }
 
-  editarPerfil() {
+  ngOnInit() {
+    this.atleta = this.atletasService.getAtletaData();
+  }
 
+  async editarPerfil() {
+    const modal = await this.modalController.create({
+      component: EditarPerfilComponent,
+      componentProps: {
+        atleta: this.atleta
+      }
+    });
+    return await modal.present();
   }
 
 }
