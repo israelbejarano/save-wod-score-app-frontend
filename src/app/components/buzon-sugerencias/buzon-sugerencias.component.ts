d343b6ecdf56982a912729962d88a84d62e599ff
israel <israel.bejarano.a@gmail.com>
2020-12-04 22:22:15 +0100

    toast enviar sugerencia ok

--- src/app/components/buzon-sugerencias/buzon-sugerencias.component.ts
+++ src/app/components/buzon-sugerencias/buzon-sugerencias.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MensajesService } from '../../services/mensajes.service';
 
 @Component({
   selector: 'app-buzon-sugerencias',
@@ -7,13 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuzonSugerenciasComponent implements OnInit {
 
+  sugerencia: string;
 
-
-  constructor() { }
+  constructor(private mensajesService: MensajesService) { }
 
   ngOnInit() {}
 
   enviar() {
+    console.log('sugerencia: ', this.sugerencia);
+    // TODO: enviar sugerencia al back cuando se envie se muestra el toast de ok o de error.
+    // de momento simulo mensaje ok a falta de implementar el back
+    const mms = 'sugerencia enviada con éxito';
+    this.mensajesService.showBottomToast(mms, 2000);
+    setTimeout(() => {
+      this.sugerencia = '';
+    }, 200);
   }
 
 }
