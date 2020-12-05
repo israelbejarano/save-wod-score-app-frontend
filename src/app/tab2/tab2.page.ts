09dfaa4c842358f1875c6d7a058794b42015eb3e
israel <israel.bejarano.a@gmail.com>
2020-12-05 17:19:51 +0100

    abrir RM

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { NewRecordComponent } from '../components/new-record/new-record.component';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,13 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(private modalController: ModalController) {}
+
+  async nuevoRm() {
+    const modal = await this.modalController.create({
+      component: NewRecordComponent,
+    });
+    return await modal.present();
+  }
 
 }
