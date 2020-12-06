4d51921c77812b49d987222c3390fae285fbac0d
israel <israel.bejarano.a@gmail.com>
2020-12-06 17:21:52 +0100

    maquetado del form

--- src/app/components/new-record/new-record.component.ts
+++ src/app/components/new-record/new-record.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { RecordsService } from '../../services/records.service';
+import { GrupoRecords } from '../../models/interfaces/grupo.records';
+import { TipoRecord } from '../../models/interfaces/tipo.record';
+
+// TODO: llevarlo a una iterfaz y a una clase
+class ModeloRecord {
+  gruposRecordsModel: GrupoRecords;
+  tipoRecordModel: TipoRecord;
+  kilos: number;
+  repeticiones: number;
+  chaleco: any;
+}
 
 @Component({
   selector: 'app-new-record',
@@ -8,12 +20,31 @@ import { ModalController } from '@ionic/angular';
 })
 export class NewRecordComponent implements OnInit {
 
-  constructor(private modalController: ModalController) { }
+  gruposRecords: GrupoRecords[];
+  tiposRecords: TipoRecord[] = [];
 
-  ngOnInit() {}
+  modeloRecord: ModeloRecord = new ModeloRecord();
+
+  constructor(private modalController: ModalController,
+              private recordsService: RecordsService) { }
+
+  ngOnInit() {
+    this.gruposRecords = this.recordsService.getGruposRecords();
+    // console.log('gruposRecords: ', this.gruposRecords);
+  }
+
+  getTiposEjercicios() {
+    // console.log('modelo grupo record: ', this.gruposRecordsModel);
+    this.tiposRecords = this.recordsService.getTiposRecordsByGrupoRecord(this.modeloRecord.gruposRecordsModel);
+  }
+
+  showFormulario() {
+    // console.log('modelo tipos de records: ', this.tipoRecordModel);
+  }
 
   guardar() {
-    this.salir();
+    console.log('modelo: ', this.modeloRecord);
+    // this.salir();
   }
 
 
