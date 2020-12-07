1e5df4457883bd76963a557eaf0d065736f761e7
israel <israel.bejarano.a@gmail.com>
2020-12-07 23:47:55 +0100

    ficheros de exportacion

---
+++ src/app/models/classes/api.classes.ts
@@ -0,0 +1,2 @@
+export * from './atleta.update.data';
+export * from './record.create.data';
---
+++ src/app/models/interfaces/api.interfaces.ts
@@ -0,0 +1,6 @@
+export * from './atleta';
+export * from './atleta.update.data';
+export * from './grupo.records';
+export * from './record.create.data';
+export * from './record';
+export * from './tipo.record';
