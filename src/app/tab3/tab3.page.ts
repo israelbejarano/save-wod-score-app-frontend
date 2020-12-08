eb04f8af3fad018255b6b73bc0bc10d3f8961867
israel <israel.bejarano.a@gmail.com>
2020-12-08 13:44:35 +0100

    header dinamico

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AtletasService } from '../services/atletas.service';
+import { IAtleta } from '../models/interfaces/api.interfaces';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+  atleta: IAtleta;
+
+  constructor(private atletasService: AtletasService) {}
+
+  ngOnInit() {
+    this.atleta = this.atletasService.getAtletaData();
+  }
 
 }
