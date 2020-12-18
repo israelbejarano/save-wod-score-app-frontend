f3d91e4dc82eb47a32ad8954cffbf1f25ff06199
israel <israel.bejarano.a@gmail.com>
2020-12-18 20:52:59 +0100

    WIP new wod component

--- src/app/components/new-wod/new-wod.component.ts
+++ src/app/components/new-wod/new-wod.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-new-wod',
@@ -7,8 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewWodComponent implements OnInit {
 
-  constructor() { }
+  puedeGuardarWod = true;
+  fRealizacion: string;
+  modeloWod: any; // TODO: crear clase e interfaz Wod
+
+  constructor(private modalController: ModalController) { }
 
   ngOnInit() {}
+
+  guardar() {
+
+    // this.salir();
+  }
+
+  guardarWod(varAValidar) {
+    // console.log('var a validar:', varAValidar);
+    if (varAValidar) {
+      this.puedeGuardarWod = true;
+    } else {
+      // this.puedeGuardarWod = false;
+    }
+  }
+
+  salir() {
+    this.modalController.dismiss();
+  }
 
 }
