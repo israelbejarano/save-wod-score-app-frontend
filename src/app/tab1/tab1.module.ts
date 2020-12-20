75969a0c4fd194b8afd198d28cca165ad74f5271
israel <israel.bejarano.a@gmail.com>
2020-12-20 14:14:53 +0100

    traducciones de componentes

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -2,6 +2,8 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+
 import { Tab1Page } from './tab1.page';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
@@ -13,6 +15,7 @@ import { SharedModule } from '../components/shared/shared.module';
     IonicModule,
     CommonModule,
     FormsModule,
+    TranslateModule,
     ComponentsModule,
     SharedModule,
     Tab1PageRoutingModule
