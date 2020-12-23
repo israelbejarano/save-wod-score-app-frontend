a431218bebb10988c0b54874b535d43d3af07f00
israel <israel.bejarano.a@gmail.com>
2020-12-23 22:56:23 +0100

    maquetado opciones ok

--- src/app/components/shared/opciones/opciones.component.ts
+++ src/app/components/shared/opciones/opciones.component.ts
@@ -7,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OpcionesComponent implements OnInit {
 
+  // TODO: traer los idiomas disponibles del LanguagesInfo
+
+  idiomaSeleccionado = 'es-ES';  // TODO: esto hacerlo dinamico con localstorage y por defecto español
+
   constructor() { }
 
   ngOnInit() {}
+
+  salir() {}
+
+  descargar() {}
+
+  cambioIdioma() {}
 
 }
