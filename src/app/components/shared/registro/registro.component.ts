16f574854a57b1a16c46ace75b1d5d96231d68f7
israel <israel.bejarano.a@gmail.com>
2020-12-26 17:27:19 +0100

    validacion de email y password

--- src/app/components/shared/registro/registro.component.ts
+++ src/app/components/shared/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Usuario } from '../../../models/classes/api.classes';
 
 @Component({
   selector: 'app-registro',
@@ -13,14 +14,31 @@ export class RegistroComponent implements OnInit {
   passwordA: string;
   passwordB: string;
 
+  showPasswordError = false;
+  showEmailError = false;
+
   constructor(private modalController: ModalController) { }
 
   ngOnInit() {}
 
-  crear() {}
+  crear() {
+    this.showPasswordError = false;
+    this.showEmailError = false;
+    if (this.passwordA.length < 7 || !/[A-Z]/.test(this.passwordA) || !/[a-z]/.test(this.passwordA) || !/\d/.test(this.passwordA)) {
+      this.showPasswordError = true;
+    }
+    if (!this.emailIsValid(this.email)) {
+      this.showEmailError = true;
+    }
+    const nuevoUsuario: Usuario = new Usuario();
+  }
 
   salir() {
     this.modalController.dismiss();
+  }
+
+  private emailIsValid(email: string) {
+    return /\S+@\S+\.\S+/.test(email);
   }
 
 }
