5aed6cd4b54597abb4e02590784edb7dd79327d8
israel <israel.bejarano.a@gmail.com>
2020-12-26 16:28:16 +0100

    funcionalidad ok del reseteo

--- src/app/components/shared/reset-password/reset-password.component.ts
+++ src/app/components/shared/reset-password/reset-password.component.ts
@@ -1,21 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { UsuariosService, MensajesService } from '../../../services/api.services';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.scss'],
 })
-export class ResetPasswordComponent implements OnInit {
+export class ResetPasswordComponent implements OnInit, OnDestroy {
 
   email: string;
   passwordA: string;
   passwordB: string;
   showErrorMms = false;
 
-  constructor(private modalController: ModalController) { }
+  nuevoPasswordSubscription: Subscription;
 
-  ngOnInit() {}
+  constructor(private modalController: ModalController,
+              private mensajesService: MensajesService,
+              private usuariosService: UsuariosService) { }
+
+
+  ngOnDestroy(): void {
+    if (this.nuevoPasswordSubscription) {
+      this.nuevoPasswordSubscription.unsubscribe();
+    }
+  }
+
+  ngOnInit() {
+    this.nuevoPasswordSubscription = this.usuariosService.nuevoPasswordEvent.subscribe(resp => {
+      console.log('resp: ', resp);
+      let mms: string;
+      if (resp === 0) {
+        mms = 'Contraseña cambiada con éxito.';
+      } else {
+        mms = 'Usuario no encontrado.';
+      }
+      setTimeout(() => {
+        this.mensajesService.showBottomToast(mms, 2000);
+        if (resp === 0) {
+          this.salir();
+        }
+      }, 2000);
+    });
+  }
 
   salir() {
     this.modalController.dismiss();
@@ -26,9 +55,9 @@ export class ResetPasswordComponent implements OnInit {
     if (this.passwordA !== this.passwordB) {
       this.showErrorMms = true;
     }
-    // TODO: servicio en atleta que manda el reseteo de password
-    // cuando devuelva ok dissmiss de este componente + toast ok
-    // si error mostrar toast con el error
+    // TODO: comprobar que el password tenga +6 longitud y aA
+    // para eso ver en otras apps mias la validación de password
+    this.usuariosService.resetearPassword(this.email, this.passwordA);
   }
 
 }
