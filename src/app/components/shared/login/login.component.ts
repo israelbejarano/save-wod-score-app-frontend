35b022bd2ab0ea31bb7a4c6cd263c5fa252b9282
israel <israel.bejarano.a@gmail.com>
2020-12-26 22:07:53 +0100

    metodo de login ok

--- src/app/components/shared/login/login.component.ts
+++ src/app/components/shared/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { RegistroComponent } from '../registro/registro.component';
+import { ResetPasswordComponent } from '../reset-password/reset-password.component';
+import { UsuariosService, MensajesService } from '../../../services/api.services';
+import { IUsuario } from '../../../models/interfaces/api.interfaces';
 
 @Component({
   selector: 'app-login',
@@ -14,23 +18,49 @@ export class LoginComponent implements OnInit {
   email: string;
   password: string;
 
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController,
+              private mensajesService: MensajesService,
+              private usuariosService: UsuariosService) { }
 
   ngOnInit() {}
 
   login() {
-    if (this.password !== '123456aA') {
-      ++this.contadorFalloPassword;
-      this.password = '';
+    const usuario: IUsuario = this.usuariosService.getUsuarioByEmail(this.email);
+    if (usuario) {
+      if (usuario.password !== this.password) {
+        if (this.password !== '123456aA') {
+          ++this.contadorFalloPassword;
+          this.password = '';
+        }
+        if (this.contadorFalloPassword >= 3) {
+          this.passwordOlvidado = true;
+        }
+        const mms = 'Email o contraseña incorrecto';
+        this.mensajesService.showBottomToast(mms, 2000);
+      } else {
+        this.modalController.dismiss();
+      }
     }
-    if (this.contadorFalloPassword >= 3) {
-      this.passwordOlvidado = true;
-    }
-    // this.modalController.dismiss();
   }
 
-  registrar() {}
+  async registrar() {
+    const modal = await this.modalController.create({
+      component: RegistroComponent,
+      componentProps: {
 
-  resetPassword() {}
+      }
+    });
+    return await modal.present();
+  }
+
+  async resetPassword() {
+    const modal = await this.modalController.create({
+      component: ResetPasswordComponent,
+      componentProps: {
+
+      }
+    });
+    return await modal.present();
+  }
 
 }
